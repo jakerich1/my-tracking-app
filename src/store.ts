fb4e8c44626535784723928a8658c5d23836f19e
Jacob Riches <jakeriches1@gmail.com>
2024-12-15 00:06:52 +0000

    Add trackingData state and setTrackingData method to manage daily tracking entries

--- src/store.ts
+++ src/store.ts
@@ -5,6 +5,24 @@ import { persist } from "zustand/middleware";
 interface CounterState {
   apiKey?: string | null;
   setApiKey: (apiKey: string) => void;
+
+  trackingData: Record<
+    string,
+    {
+      value: number;
+      description: string;
+      key: string;
+      dateString: string;
+    }[]
+  >;
+
+  setTrackingData: (
+    dateIso: string,
+    key: string,
+    value: number,
+    description: string,
+    dateString: string
+  ) => void;
 }
 
 // Create the store with persist middleware
@@ -13,6 +31,28 @@ const useStore = create<CounterState>()(
     (set) => ({
       apiKey: null,
       setApiKey: (apiKey) => set({ apiKey }),
+
+      trackingData: {},
+      setTrackingData: (dateIsoKey, key, value, description, dateString) =>
+        set((state) => {
+          const currentDayData = state.trackingData[dateIsoKey] || [];
+          const updatedDayData = [
+            ...currentDayData,
+            {
+              key,
+              value,
+              description,
+              dateString,
+            },
+          ];
+
+          return {
+            trackingData: {
+              ...state.trackingData,
+              [dateIsoKey]: updatedDayData,
+            },
+          };
+        }),
     }),
     {
       name: "my-app-store", // Key for localStorage
